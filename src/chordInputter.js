cbc9d7d27ed8f80d9ee5cd71e094734fc6f21f43
Firas <firas.layani@gmail.com>
2025-02-08 02:23:21 +0000

    handled empty numeral input, not exhaustive handling, attempted the strart of chord progression building

--- src/chordInputter.js
+++ src/chordInputter.js
@@ -1,21 +1,22 @@
 import { getChordV3 } from './musicHelpers.js';
 import { newPlayChord } from './musicHelpers.js';
 import { useMusicalKeyContext } from './musicalKeyContext.js';
+import { useChordContext } from './chordOrderContext';
+import { ChordButton } from './musicHelpers.js';
 
 export default function TextBoxComponent({ text, setText }) {
   // Access musicalKey and setMusicalKey from MusicalKeyContext
   const { musicalKey, setMusicalKey } = useMusicalKeyContext();
 
-  // Function to show what text was in the box when clicked
-  function showText() {
-    alert(`You typed ${text}`);
-  }
+  // Access chordOrder and setChordOrder from ChordContext
+  const { chordOrder, setChordOrder } = useChordContext();
 
   // Function that runs whenever a key is pressed while in the text box
   function handleKeyDown(e) {
     if (e.key === 'Enter') {
-      alert(`Man pressed Enter still. Anyway you entered ${text}. Chord in scale is ${getChordV3(musicalKey, text)}`);
-      newPlayChord(getChordV3(musicalKey, text));
+      //alert(`Man pressed Enter still. Anyway you entered ${text}. Chord in scale is ${getChordV3(musicalKey, text)}`);
+      newPlayChord(getChordV3(musicalKey, text === '' ? 'I' : text));
+      //chordOrder.push(ChordButton(text, getChordV3(musicalKey, text)));
       setText('');
     }
   }
